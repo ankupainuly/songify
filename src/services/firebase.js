5a5f0cf11c74af736f087a35c16a59692383c9b2
Ankur Painuly <73772025+ankupainuly@users.noreply.github.com>
2024-07-30 10:15:01 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,14 +4,7 @@ import "@firebase/database";
 import "@firebase/storage";
 
 const config ={
-    apiKey: "AIzaSyBOmt3i_Nx4ebb3VFlIh9DO3euHDtl6N4I",
-    authDomain: "songify-a8613.firebaseapp.com",
-    databaseURL: "https://songify-a8613-default-rtdb.firebaseio.com",
-    projectId: "songify-a8613",
-    storageBucket: "songify-a8613.appspot.com",
-    messagingSenderId: "139169318796",
-    appId: "1:139169318796:web:8247e98649ec8b9d5838a9",
-    measurementId: "G-1NYKVDQJG1"
+    the config info. you get from your fir base project
 };
 
 firebase.initializeApp(config);
